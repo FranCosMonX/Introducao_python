af6c277815d2958a46b9c11a1f6b7e1f7fd5aff4
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-01-27 11:28:22 -0300

    Conclusão da atividade 06 da proz sobre listas

---
+++ Atividade06.py
@@ -0,0 +1,6 @@
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa']
+
+print(f"Quantidade de elementos: {len(lista_musicos)}.")
+print(f'lista_musicos[2] = {lista_musicos[2]}.')
+print(f'lista_musicos[9] = {lista_musicos[9]}.')
+print(f'lista_musicos[14] = {lista_musicos[14]}.')
