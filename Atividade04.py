6712f250b93b4053b6feb6171d336d0c9c93be25
MaskQuanly <debian.programmer.francisco@gmail.com>
2024-01-27 11:00:40 -0300

    Inserindo try except na atividade 04

--- Atividade04.py
+++ Atividade04.py
@@ -1,9 +1,10 @@
 nome = input('informe seu nome completo: ')
-ano = int(input('informe o ano em que nasceu: '))
-
+ano = 0
 
 while ano < 1922 or ano > 2021:
-    ano = int(input('Informe um ano válido: '))
-    
+    try:
+        ano = int(input('Informe um ano válido: '))
+    except:
+        print('É aceito apenas número inteiro. Por favor, tente novamente.')
 
 print(f'olá {nome}, você fará {2022 - ano} anos de idade em 2022 (ano atual).')
